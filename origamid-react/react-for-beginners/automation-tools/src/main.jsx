2100daf92a5b7a373e522c2c365194e3c7268459
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-06-06 17:25:31 -0300

    :pencil: class(react-for-begginers): transpilers, bundlers

---
+++ origamid-react/react-for-beginners/automation-tools/src/main.jsx
@@ -0,0 +1,10 @@
+// Building react app
+import ReactDOM  from "react-dom/client";
+import React from "react";
+
+// Import component
+import App from "./App";
+
+
+const root = document.getElementById('root');
+ReactDOM.createRoot(root).render(<App />);
