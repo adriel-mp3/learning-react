15159901a0ff83226550101c17df88962221e3b9
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-06-07 18:47:11 -0300

    :pencil: class(react-for-beginners): components example

---
+++ origamid-react/react-for-beginners/components/src/App.jsx
@@ -0,0 +1,14 @@
+import Form from "../Form/Form";
+import Header from "./Header";
+import Footer from "./Footer";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Form />
+      <Footer />
+    </>
+  );
+}
---
+++ origamid-react/react-for-beginners/components/src/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = () => {
+  return <header style={{marginBottom: '20px'}}>Header</header>
+}
+
+export default Header;
