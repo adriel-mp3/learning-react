703b4daf8a1d0a023ef8f53850720d4b1a257946
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-06-07 12:54:58 -0300

    :pencil: exercise(react-for-beginners): JSX exercise

---
+++ origamid-react/react-for-beginners/JSX/exercise.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+const luana = {
+  cliente: "Luana",
+  idade: 27,
+  compras: [
+    { nome: "Notebook", preco: "R$ 2500" },
+    { nome: "Geladeira", preco: "R$ 3000" },
+    { nome: "Smartphone", preco: "R$ 1500" },
+  ],
+  ativa: true,
+};
+
+const mario = {
+  cliente: "Mario",
+  idade: 31,
+  compras: [
+    { nome: "Notebook", preco: "R$ 2500" },
+    { nome: "Geladeira", preco: "R$ 3000" },
+    { nome: "Smartphone", preco: "R$ 1500" },
+    { nome: "Guitarra", preco: "R$ 3500" },
+  ],
+  ativa: false,
+};
+const totalItens = (compras) => {
+  const total = compras.reduce((acc, item) => {
+    return Number(item.preco.slice(3)) + acc;
+  }, 0);
+  return total;
+};
+
+const App = () => {
+  const dados = luana;
+  return (
+    <ul>
+      <li>Nome: {dados.cliente}</li>
+      <li>Idade: {dados.idade}</li>
+      <li>
+        Situação:{" "}
+        <span style={{ color: dados.ativa ? "green" : "red" }}>
+          {dados.ativa ? "Ativa" : "Inativa"}
+        </span>
+      </li>
+      <li>
+        Total gasto:
+        {totalItens(dados.compras)}
+      </li>
+      {totalItens(dados.compras) > 10000 && <li>Você está gastando muito.</li>}
+    </ul>
+  );
+};
+
+export default App;
