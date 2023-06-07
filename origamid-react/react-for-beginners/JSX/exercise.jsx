612ad728dfaaade45c33c00e0816ffb1bca4b234
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-06-07 12:56:31 -0300

    :pencil: exercise(react-for-beginners): JSX exercise

--- origamid-react/react-for-beginners/JSX/exercise.jsx
+++ origamid-react/react-for-beginners/JSX/exercise.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
+// Mostre os dados da aplicação, como aprensetado no vídeo
+// Não utilize CSS externo, use o style para mudar as cores
+// Se a situação estiver ativa pinte de verde, inativa vermelho
+// Se o gasto for maior que 10000 mostre uma mensagem
+
 const luana = {
   cliente: "Luana",
   idade: 27,
