6aa21c64af412d00e666da09673a00aa47c8edfd
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-06-08 16:18:14 -0300

    :heavy_check_mark: challenge(your-first-components): solution for your own component

---
+++ react-doc/learn-react/describing-the-ui/your-first-component/some-challenges/4-your-own-component/App.jsx
@@ -0,0 +1,11 @@
+/* 
+Write a component from scratch. You can give it any valid name and return any markup. If you’re out of ideas, you can write a Congratulations component that shows <h1>Good job!</h1>. Don’t forget to export it!
+**/
+
+// Write your component below!
+
+const Hello = () => {
+  return <h1>Hello World</h1>
+}
+
+export default Hello;
